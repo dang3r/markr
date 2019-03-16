303a6e5614084aebaeee53e164c647bd442e8607
Daniel Cardoza <danielpcardoza@gmail.com>
2019-03-15 22:36:51 -0700

    Add decoders for different external attributes encodings (#4)
    Attribute values are just raw bytes and do not have a standard encoding.
    Add two decoders for utf8 and plist for Mac. If a suitable decoder, is not found,
    throw an exception.

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,5 +1,7 @@
-import markr
 import os
+import plistlib
+
+import markr
 
 def test_set_get(tmpdir):
     f = tmpdir.join('foo.txt')
@@ -50,3 +52,16 @@ def test_rm(tmpdir):
 
     assert len(markr.get(f_name)) == 0
 
+
+def test_plist_key(tmpdir):
+    f = tmpdir.join('foo.txt')
+    f.write('l')
+    markr.setb(
+        str(f), 
+        'com.apple.metadata:kMDItemWhereFroms',
+        b'bplist00\xa2\x01\x02_\x10Ghttps://www.cs.cornell.edu/courses/cs6410/2013fa/slides/18-lamport.pptx_\x10\x17https://www.google.com/\x08\x0bU\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00o')
+    attrs = markr.get(str(f))
+    assert len(attrs) == 1
+    assert attrs[0] == (
+        'com.apple.metadata:kMDItemWhereFroms',
+        "['https://www.cs.cornell.edu/courses/cs6410/2013fa/slides/18-lamport.pptx', 'https://www.google.com/']")
